6b7cf671320831d074fab105d59ab444b2cc35ca
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-12 21:14:49 +0700

    再び！

--- Hotel Price App/View/Home.swift
+++ Hotel Price App/View/Home.swift
@@ -42,15 +42,58 @@ struct Home: View {
             .padding([.horizontal, .bottom])
             .padding(.top, 10)
             
-            HStack {
-                Text("Recommended")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(white)
+            ScrollView(.vertical, showsIndicators: false) {
                 
-                Spacer()
+                VStack {
+                    
+                    HStack {
+                        Text("Recommended")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(white)
+                        
+                        Spacer()
+                    }
+                    .padding()
+                    
+                    HStack {
+                        
+                        VStack(alignment: .leading, spacing: 6) {
+                            
+                            Text("Baglioni Hotel")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                            
+                            HStack {
+                                Text("Only Today")
+                                    .foregroundColor(white)
+                                
+                                Text("50%")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                
+                                Text("off")
+                                    .foregroundColor(white)
+                            }
+                            
+                            Text("$500,23")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Spacer(minLength: 5)
+                        
+                        Image("p2").resizable()
+                        
+                    }
+                    .padding([.vertical, .leading])
+                    .background (
+                        LinearGradient(gradient: .init(colors: [Color("g1"), Color("g2")]), startPoint: .top, endPoint: .bottom)
+                            .cornerRadius(25)
+                    )
+                    .padding()
+                }
             }
-            .padding()
         }
     }
 }
